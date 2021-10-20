d78502f901f4108e67b2d692c4e7b1b3d4901729
Amy Dang <kirontoo@gmail.com>
2021-10-19 18:47:29 -0700

    Convert to tsx file

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
+function HomePage() {
+    return (
+        <ChakraProvider>
+            <h1>Habimake</h1>
+        </ChakraProvider>
+    )
+}
+
+export default HomePage
+
