6b35638fffc3802a3bc7106dfe2c022f4f80ab02
Amy Dang <kirontoo@gmail.com>
2021-11-02 20:30:42 -0700

    Separate the logo and the text

---
+++ components/Brand.tsx
@@ -0,0 +1,20 @@
+import {
+    Box,
+    Center,
+    Text,
+    HStack,
+    Link
+} from "@chakra-ui/react";
+import Logo from "./Logo";
+
+function Brand() {
+    return (
+        <HStack fontSize="2xl">
+            <Logo/>
+            <Text color="white"  as="strong" align="center">HABIMAKE</Text>
+        </HStack>
+    );
+}
+
+export default Brand;
+
