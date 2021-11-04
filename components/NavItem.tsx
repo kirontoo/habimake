9ea248a793304ca135773385b0cf728b601915ef
Amy Dang <kirontoo@gmail.com>
2021-11-03 18:43:09 -0700

    Refactor to RouterLink

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,36 +1,51 @@
 import {
-    Flex,
-    Link,
-    FlexProps,
-} from '@chakra-ui/react';
-import { ReactText } from 'react';
-import NextLink from 'next/link';
+    VStack,
+    Text,
+    StackProps,
+} from "@chakra-ui/react";
+import { ReactText } from "react";
 
-interface NavItemProps extends FlexProps {
+import RouterLink from "./RouterLink";
+
+interface NavItemProps extends StackProps {
     children: ReactText;
     href: string;
+    index: number;
 }
 
-function NavItem ({ children, href, ...rest }: NavItemProps) {
+function NavItem ({ children, href, index, ...rest }: NavItemProps) {
     return (
-        <NextLink href={href}>
-            <Link style={{ textDecoration: 'none' }}>
-                <Flex
-                    align="center"
-                    p="4"
-                    mx="4"
-                    role="group"
-                    cursor="pointer"
-                    _hover={{
-                        color: 'cyan.400',
-                        borderBottom: "1px",
-                        borderBottomColor: "cyan.400",
-                    }}
-                    {...rest}>
-                    {children}
-                </Flex>
-            </Link>
-        </NextLink>
+        <RouterLink 
+            as="li"
+            href={href}
+            fontSize="2xl"
+            style={{  listStyle: "none" }}
+            _hover={{
+                textDecoration: "none"
+            }}
+            position="relative"
+        >
+            <VStack
+                as="a"
+                href={href}
+                align="flex-start"
+                p="4"
+                mx="4"
+                role="group"
+                cursor="pointer"
+                _hover={{
+                    color: 'cyan.400',
+                    borderBottom: "1px",
+                    borderBottomColor: "cyan.400",
+                }}
+                {...rest}>
+                <Text 
+                    fontSize="lg"
+                    color="cyan.400"
+                >0{index}.</Text>
+                <Text>{children}</Text>
+            </VStack>
+        </RouterLink>
     );
 };
 
