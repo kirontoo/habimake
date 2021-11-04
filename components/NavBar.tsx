a5dcc886ca863b7e5ffd2df7f3c30a6735eb3131
Amy Dang <kirontoo@gmail.com>
2021-11-03 18:41:50 -0700

    Add animation on menu button hover

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,27 +1,28 @@
-import  { 
-    HamburgerIcon ,
-} from '@chakra-ui/icons';
+import  { HamburgerIcon } from "@chakra-ui/icons";
 import {
     IconButton,
     Flex,
     Spacer,
     Center,
-    FlexProps
+    FlexProps,
+    IconButtonProps
 } from '@chakra-ui/react';
+import { motion } from "framer-motion";
 
-import Logo from "components/Logo";
+import Brand from "components/Brand";
 
 interface NavProps extends FlexProps {
     onOpen: () => void
 }
 
 function Navbar({onOpen, ...rest}: NavProps) {
+    const MotionMenuButton = motion<IconButtonProps>(IconButton)
     return (
         <Flex {...rest}>
-            <Logo/>
+            <Brand/>
             <Spacer/>
             <Center>
-                <IconButton
+                <MotionMenuButton
                     onClick={onOpen}
                     size="lg"
                     aria-label="open menu"
@@ -31,6 +32,15 @@ function Navbar({onOpen, ...rest}: NavProps) {
                         border: "2px",
                         borderStyle: "dashed",
                         borderColor: "cyan.400",
+                    }}
+                    whileHover={{ 
+                        rotate: 180,
+                        transition: { 
+                            easeIn: "linear",
+                            easeOut: "linear",
+                            duration: 0.2, 
+                            delay: 0 
+                        }
                     }}
                 />
             </Center>
