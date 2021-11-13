b6167f0717cdc0a64c3f89929dfc732d095fbb5d
Amy Dang <kirontoo@gmail.com>
2021-11-13 14:55:23 -0800

    Add schemas for user authentication

---
+++ lib/Schema.ts
@@ -0,0 +1,23 @@
+import * as Yup from "yup";
+
+export module AuthSchema {
+    export const Password = Yup.string()
+        .required('Required')
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+        );
+
+    export const Email= Yup.string()
+        .email('Invalid email')
+        .required('Required');
+
+    export const Username = Yup.string().min(4).max(30);
+
+    // export type AuthUserForm = {
+    //     email: string,
+    //     password: string,
+    //     username?: string,
+    //     verifyPassword?: string
+    // };
+}
