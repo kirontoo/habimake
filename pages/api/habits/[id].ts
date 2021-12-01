61aa85274f9be770e22d0128d2d5e9bcbb799b84
Amy Dang <kirontoo@gmail.com>
2021-11-30 19:15:33 -0800

    Refactor

--- pages/api/habits/[id].ts
+++ pages/api/habits/[id].ts
@@ -1,12 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { supabase } from "lib/supabaseClient";
 import apiHandler from "lib/helpers/apiHandler";
+import parseToken from "lib/helpers/parseToken";
 
 async function habitHandler (
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const { method } = req;
+    const TABLE = "Habit";
+
+    let token = parseToken(req.headers.authorization);
+    supabase.auth.setAuth(token);
+
     switch (method) {
         case 'GET':
             return await GetHabitById(req, res);
@@ -15,90 +21,73 @@ async function habitHandler (
         case 'DELETE':
             return await DeleteHabit(req, res);
         default:
-        res.setHeader('Allow', ['GET', 'PATCH', 'DELETE']);
-        res.status(405).end(`Method ${method} Not Allowed`)
+            res.setHeader('Allow', ['GET', 'PATCH', 'DELETE']);
+            res.status(405).end(`Method ${method} Not Allowed`)
     }
-}
 
-async function GetHabitById(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
+    async function GetHabitById( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const habitId = req.query.id;
 
-        let token = req.headers.authorization.split(" ")[1];
-        supabase.auth.setAuth(token);
+            const { data: habit, error } = await supabase
+                .from(TABLE)
+                .select()
+                .eq("id", habitId)
+                .single();
 
-        const habitId = req.query.id;
+            if (error) {
+                return res.status(401).json({ message: 'Invalid Token' });
+            }
 
-        const { data: habit, error } = await supabase
-            .from("Habit")
-            .select()
-            .eq("id", habitId)
-            .single();
-
-        if (error) {
-            return res.status(401).json({ message: 'Invalid Token' });
+            return res.status(200).json(habit);
+        } catch (error) {
+            return res.status(500).send({
+                error: error.message
+            });
         }
-
-        return res.status(200).json(habit);
-    } catch (error) {
-        return res.status(500).send({
-            error: error.message
-        });
     }
-}
 
-async function UpdateHabit(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
-        let token = req.headers.authorization.split(" ")[1];
-        supabase.auth.setAuth(token);
+    async function UpdateHabit( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const habitId = req.query.id;
 
-        const habitId = req.query.id;
-        const { data: habit, error } = await supabase
-            .from("Habit")
-            .update({...req.body})
-            .eq("id", habitId);
+            const { data: habit, error } = await supabase
+                .from(TABLE)
+                .update({...req.body})
+                .eq("id", habitId);
 
-        if (error) {
-            throw error;
+            if (error) {
+                throw error;
+            }
+
+            return res.status(200).json({habit, error});
+        } catch (error) {
+            return res.status(500).send({
+                error: error.message
+            });
         }
-
-        return res.status(200).json({habit, error});
-    } catch (error) {
-        return res.status(500).send({
-            error: error.message
-        });
     }
-}
 
-async function DeleteHabit(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
-        const habitId = req.query.id;
+    async function DeleteHabit( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const habitId = req.query.id;
 
-        let token = req.headers.authorization.split(" ")[1];
-        supabase.auth.setAuth(token);
+            // check if owner before deleting
+            const { data, error } = await supabase
+                .from(TABLE)
+                .delete()
+                .eq("id", Number.parseInt(String(habitId)));
 
-        // check if owner before deleting
-        const { data, error } = await supabase.from("Habit")
-            .delete()
-            .eq("id", Number.parseInt(String(habitId)));
+            if ( error ) {
+                throw new Error(error.message);
+            }
 
-        if ( error ) {
-            throw new Error(error.message);
+            return res.status(200).json(data);
+        } catch (error) {
+            return res.status(500).send({
+                error: error.message
+            });
         }
-
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).send({
-            error: error.message
-        });
     }
 }
 
--- pages/api/habits/index.ts
+++ pages/api/habits/index.ts
@@ -1,83 +1,75 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "lib/prisma";
-import { Prisma, Habit } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { supabase } from "lib/supabaseClient";
 import apiHandler from "lib/helpers/apiHandler";
 import jwt from "jsonwebtoken";
+import parseToken from "lib/helpers/parseToken";
 
 async function habitHandler (
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const { method } = req;
+    let token = parseToken(req.headers.authorization);
+    supabase.auth.setAuth(token);
+
     switch (method) {
         case 'GET':
             return await GetHabits(req, res);
         case 'POST':
             return await CreateHabit(req, res);
         default:
-        res.setHeader('Allow', ['GET', 'POST']);
-        res.status(405).end(`Method ${method} Not Allowed`)
+            res.setHeader('Allow', ['GET', 'POST']);
+            res.status(405).end(`Method ${method} Not Allowed`)
     }
-}
 
-// Get all habits from current user
-async function GetHabits(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
-        let token = req.headers.authorization.split(" ")[1];
-        supabase.auth.setAuth(token);
+    // Get all habits from current user
+    async function GetHabits( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const { data: habits, error } = await supabase
+                .from("Habit")
+                .select("*")
 
-        const { data: habits, error } = await supabase
-            .from("Habit")
-            .select("*")
+            if (error) {
+                return res.status(400).send("Bad Request");
+            }
 
-        if (error) {
-            return res.status(400).send("Bad Request");
+            return res.status(200).json({ habits });
+        } catch (error) {
+            return res.status(500).send({
+                error: error.message
+            });
         }
-
-       return res.status(200).json({ habits });
-    } catch (error) {
-        return res.status(500).send({
-            error: error.message
-        });
     }
-}
 
-async function CreateHabit(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    try {
-        let token = req.headers.authorization.split(" ")[1];
-        supabase.auth.setAuth(token);
+    async function CreateHabit( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            let user = jwt.verify(token, process.env.JWT_SECRET);
 
-        let user = jwt.verify(token, process.env.JWT_SECRET);
+            const data: Prisma.HabitCreateInput = {
+                ...req.body,
+                userId: user.sub,
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            };
 
-        const data: Prisma.HabitCreateInput = {
-            ...req.body,
-            userId: user.sub,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString()
-        };
+            // This client will now send requests as this user
+            const { data: habit, error } = await supabase
+                .from("Habit")
+                .insert([{...data}]);
 
-        // This client will now send requests as this user
-        const { data: habit, error } = await supabase
-            .from("Habit")
-            .insert([{...data}]);
+            if (error) {
+                return res.status(401).send('Unauthorized');
+            }
 
-        if (error) {
-            return res.status(401).send('Unauthorized');
+            return res.status(200).json(habit);
+        } catch (error) {
+            return res.status(500).send({
+                error: error.message
+            });
         }
-
-        return res.status(200).json(habit);
-    } catch (error) {
-        return res.status(500).send({
-            error: error.message
-        });
     }
 }
+
 
 export default apiHandler(habitHandler);
