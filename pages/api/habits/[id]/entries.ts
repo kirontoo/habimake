b38fff690df3a11ff7d91c196732060a880a5763
Amy Dang <kirontoo@gmail.com>
2021-12-02 19:13:35 -0800

    Prevent duplicate habit entry creation

--- pages/api/habits/[id]/entries.ts
+++ pages/api/habits/[id]/entries.ts
@@ -3,6 +3,8 @@ import { Prisma } from "@prisma/client";
 import { supabase } from "lib/supabaseClient";
 import apiHandler from "lib/helpers/apiHandler";
 import parseToken from "lib/helpers/parseToken";
+import prisma from "lib/prisma";
+import { DateTime } from "luxon";
 
 async function handler( req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
@@ -25,22 +27,46 @@ async function handler( req: NextApiRequest, res: NextApiResponse) {
 
     // TODO: get habit entries based on month using the query string
     
-    async function CreateHabitEntry( req: NextApiRequest, res, NextApiResponse ) {
+    async function CreateHabitEntry( req: NextApiRequest, res: NextApiResponse ) {
         try {
-            // TODO: check if the an entry already exists for today
-
             const habitId = req.query.id;
-            const data: Prisma.HabitEntryCreateInput = {
+            
+            const now = DateTime.now();
+            const yesterday = now.minus({day: 1}).toISO();
+            
+            const { data: prevEntry } = await supabase
+                .from(TABLE)
+                .select()
+                .eq("habitId", habitId)
+                .lt("createdAt", now.toISO())
+                .gt("createdAt", yesterday)
+                .limit(1)
+                .single()
+
+            if ( prevEntry ) {
+                const entryDate = DateTime.fromISO(prevEntry.createdAt);
+
+                if (entryDate.day == now.day && entryDate.year == now.year && entryDate.month == now.month) {
+                    // entry for today already exists do not create another one
+                    return res.status(200).json(prevEntry);
+                }
+            }
+
+            const data = {
                 habitId, 
-                createdAt: new Date().toISOString(),
+                createdAt: now.toISO(),
             };
 
-            const { data: entry, error } = await supabase
+            const { data: habitEntry, error } = await supabase
                 .from(TABLE)
                 .insert([{...data}])
                 .single();
 
-            return res.status(200).json(entry);
+            if (error) {
+                throw error;
+            }
+
+            return res.status(200).json(habitEntry);
         } catch(error) {
             return res.status(500).send({
                 error: error.message
