b5b9c0e04ead1f8aea3a1d3b8c7b17c7e66297a9
Amy Dang <kirontoo@gmail.com>
2021-12-04 14:49:48 -0800

    Get habit entries based on month and year

--- pages/api/habits/[id]/entries.ts
+++ pages/api/habits/[id]/entries.ts
@@ -4,6 +4,14 @@ import apiHandler from "lib/helpers/apiHandler";
 import parseToken from "lib/helpers/parseToken";
 import { DateTime } from "luxon";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '500kb',
+    },
+  },
+}
+
 async function handler( req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
     const TABLE = "HabitEntry";
@@ -13,7 +21,7 @@ async function handler( req: NextApiRequest, res: NextApiResponse) {
 
     switch (method) {
         case 'GET':
-            return res.status(200).send("get entry")
+            return await GetHabitEntries(req, res);
         case 'POST':
             return await CreateHabitEntry(req, res);
         case 'DELETE':
@@ -23,16 +31,77 @@ async function handler( req: NextApiRequest, res: NextApiResponse) {
             res.status(405).end(`Method ${method} Not Allowed`)
     }
 
-    // TODO: get habit entries based on month using the query string
+    /*
+    *  Query different months by using query strings
+    *  Example: /api/habits/20/:id/entries?months=3,5&year=2020
+    */
+    async function GetHabitEntries( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const habitId = req.query.id;
+            let startDate = DateTime.now();
+            let endDate = startDate.endOf("year");
+            let habit = null;
+
+            let months = req.query.months ? req.query.months.toString().split(",") : null;
+            let year = req.query.year ?? null;
+
+            if ( !year ) {
+                // set year to current year
+                year = startDate.year;
+            }
+
+            if ( !months ) {
+                // No month queries -> get entries from the last 2 months?
+                startDate = startDate.minus({ months: 2 });
+            } else if ( months.length > 1 ) {
+                // Range Month queries (1-3) -> get entries from months 1 - 3
+                startDate = DateTime.fromObject({ month: months[0], year });
+                endDate = DateTime.fromObject({ month: months[1], year }).endOf('month');
+            } else {
+                // Query from a month -> get entries from month til end of year
+                startDate = DateTime.fromObject({ month: months[0], year });
+                endDate = startDate.endOf('year');
+            }
+
+            habit = await supabase
+                .from("Habit")
+                .select("*, HabitEntry!inner(*)")
+                .eq("id", habitId)
+                .gte("HabitEntry.createdAt", startDate.toISO())
+                .lte("HabitEntry.createdAt", endDate.toISO())
+                .limit(1)
+                .single();
+
+            if (habit === null) {
+                // user is not the owner
+                return res.status(403).end("Not authorized")
+            }
+
+            return res.status(200).json({...habit.data, startDate, endDate})
+
+        } catch(error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+    }
     
 
-    // TODO: check for owner
+    // TODO: increase habit streak
     async function CreateHabitEntry( req: NextApiRequest, res: NextApiResponse ) {
         try {
             const habitId = req.query.id;
             
             const now = DateTime.now();
             const yesterday = now.minus({day: 1}).toISO();
+
+            // NOTE: payload should include the date
+            const { createdAt } = req.body
+
+            let validDate = DateTime.fromISO(createdAt).invalid;
+            if (!createdAt || validDate) {
+                return res.status(400).end("Invalid Payload");
+            }
 
             const { data: habit } = await supabase
                 .from("Habit")
