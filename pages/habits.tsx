ac133338a3fff6cf1d5351da0d3dc279cec17054
Amy Dang <kirontoo@gmail.com>
2021-12-05 15:49:02 -0800

    Setting up habits page

--- pages/habits.tsx
+++ pages/habits.tsx
@@ -1,7 +1,41 @@
+import {
+    VStack,
+    HStack,
+    Flex,
+    Box,
+    Stack,
+    Wrap,
+    IconButton,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from "@chakra-ui/react";
+import { DateTime } from "luxon";
+import {
+    useState,
+    useEffect
+} from "react";
+import EntryButton from "components/EntryButton";
+
 function Habits() {
+    let date = DateTime.now();
+    let [ currentMonth, setMonth ] = useState(date.month);
+
+    function onMark() {
+        alert("marked")
+    }
+
     return (
-        <h3>habits</h3>
-    )
+        <>
+            <EntryButton isChecked onClick={onMark}/>
+            <EntryButton/>
+        </>
+    );
 }
 
 export default Habits;
