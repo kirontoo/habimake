4c8e36b35ce595a501365ed96a023d0056fbc93d
Amy Dang <kirontoo@gmail.com>
2021-12-05 14:09:48 -0800

    Change api route to users

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,31 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "lib/prisma";
-import { Prisma } from "@prisma/client";
 import { supabase } from "lib/supabaseClient";
 import apiHandler from "lib/helpers/apiHandler";
+import parseToken from "lib/helpers/parseToken";
 
 async function userHandler (
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const TABLE = "User";
     const { method } = req;
+    let token = parseToken(req.headers.authorization);
+    supabase.auth.setAuth(token);
 
     switch (method) {
         case 'GET':
             return await GetUser(req, res);
+        case 'DELETE':
+            return await DeleteUser(req, res);
+        case 'PATCH':
+        return res.status(200).end();
         default:
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end('Method ${method} Not Allowed')
+            res.setHeader('Allow', ['GET', 'PATCH', 'DELETE']);
+            res.status(405).end('Method ${method} Not Allowed')
     }
 
     async function GetUser( req: NextApiRequest, res: NextApiResponse ) {
         try {
-            let token = req.headers.authorization.split(" ")[1];
-            supabase.auth.setAuth(token);
-
             const id = req.query.id;
             let { data, error } = await supabase
-                .from("User")
+                .from(TABLE)
                 .select()
                 .eq("id", id)
                 .single()
@@ -36,6 +39,24 @@ async function userHandler (
 
             return res.status(200).json(data);
         } catch(error) {
+            return res.status(500).send({
+                error: error.message
+            });
+        }
+    }
+
+    async function DeleteUser( req: NextApiRequest, res: NextApiResponse ) {
+        try {
+            const id = req.query.id;
+            await supabase
+                .from(TABLE)
+                .delete()
+                .eq("id", id)
+                .limit(1)
+                .single()
+
+            return res.status(200).end();
+        } catch (error) {
             return res.status(500).send({
                 error: error.message
             });
